9c15121bba728c3f9a8a5d72b2e0085bc3984a66
Ivan Udovin <wilcot@ya.ru>
2019-08-10 16:28:36 +0300

    Add start stop test

--- core/app_test.go
+++ core/app_test.go
@@ -9,20 +9,20 @@ import (
 func TestNewApp(t *testing.T) {
 	var cfg config.Config
 	if _, err := NewApp(&cfg); err == nil {
-		t.Error("Expected error while creating app")
+		t.Fatal("Expected error while creating app")
 	}
 	cfg.Database = config.DatabaseConfig{
 		Driver: config.SQLiteDriver,
 	}
 	if _, err := NewApp(&cfg); err == nil {
-		t.Error("Expected error while creating app")
+		t.Fatal("Expected error while creating app")
 	}
 	cfg.Database = config.DatabaseConfig{
 		Driver:  config.SQLiteDriver,
 		Options: config.SQLiteOptions{Path: "?mode=memory"},
 	}
 	if _, err := NewApp(&cfg); err == nil {
-		t.Error("Expected error while creating app")
+		t.Fatal("Expected error while creating app")
 	}
 	cfg.Security = config.SecurityConfig{
 		PasswordSalt: config.Secret{
@@ -31,6 +31,29 @@ func TestNewApp(t *testing.T) {
 		},
 	}
 	if _, err := NewApp(&cfg); err != nil {
-		t.Error("Error: ", err)
+		t.Fatal("Error: ", err)
 	}
+}
+
+func TestApp_StartStop(t *testing.T) {
+	cfg := config.Config{
+		Database: config.DatabaseConfig{
+			Driver:  config.SQLiteDriver,
+			Options: config.SQLiteOptions{Path: "?mode=memory"},
+		},
+		Security: config.SecurityConfig{
+			PasswordSalt: config.Secret{
+				Type: config.DataSecret,
+				Data: "qwerty123",
+			},
+		},
+	}
+	app, err := NewApp(&cfg)
+	if err != nil {
+		t.Fatal("Error: ", err)
+	}
+	if err := app.Start(); err != nil {
+		t.Fatal("Error: ", err)
+	}
+	app.Stop()
 }

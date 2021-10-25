dbdafe510eb8b1c5e8713b1bcb5bd0f1840344d9
Ivan Udovin <wilcot@ya.ru>
2021-10-25 16:38:00 +0300

    Create test client for api

---
+++ api/base_test.go
@@ -0,0 +1,127 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+)
+
+type testClient struct {
+	cookies []*http.Cookie
+}
+
+func newTestClient() *testClient {
+	return &testClient{}
+}
+
+func (c *testClient) setCookie(req *http.Request) {
+	for _, cookie := range c.cookies {
+		req.AddCookie(cookie)
+	}
+}
+
+func (c *testClient) Register(login, password string) (User, error) {
+	data, err := json.Marshal(map[string]string{
+		"login":       login,
+		"password":    password,
+		"email":       "test@example.com",
+		"first_name":  "First",
+		"last_name":   "Last",
+		"middle_name": "Middle",
+	})
+	if err != nil {
+		return User{}, err
+	}
+	req := httptest.NewRequest(
+		http.MethodPost, "/api/v0/register", bytes.NewReader(data),
+	)
+	req.Header.Add("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	if err := testHandler(req, rec); err != nil {
+		return User{}, err
+	}
+	if rec.Code != http.StatusCreated {
+		var resp errorResp
+		if err := json.NewDecoder(rec.Body).Decode(&resp); err != nil {
+			return User{}, err
+		}
+		return User{}, &resp
+	}
+	var resp User
+	if err := json.NewDecoder(rec.Body).Decode(&resp); err != nil {
+		return User{}, err
+	}
+	return resp, nil
+}
+
+func (c *testClient) Login(login, password string) (Session, error) {
+	data, err := json.Marshal(map[string]string{
+		"login":    login,
+		"password": password,
+	})
+	if err != nil {
+		return Session{}, err
+	}
+	req := httptest.NewRequest(
+		http.MethodPost, "/api/v0/login", bytes.NewReader(data),
+	)
+	c.setCookie(req)
+	req.Header.Add("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	if err := testHandler(req, rec); err != nil {
+		return Session{}, err
+	}
+	if rec.Code != http.StatusCreated {
+		var resp errorResp
+		if err := json.NewDecoder(rec.Body).Decode(&resp); err != nil {
+			return Session{}, err
+		}
+		return Session{}, &resp
+	}
+	var resp Session
+	if err := json.NewDecoder(rec.Body).Decode(&resp); err != nil {
+		return Session{}, err
+	}
+	c.cookies = rec.Result().Cookies()
+	return resp, nil
+}
+
+func (c *testClient) Logout() error {
+	req := httptest.NewRequest(http.MethodPost, "/api/v0/logout", nil)
+	c.setCookie(req)
+	rec := httptest.NewRecorder()
+	if err := testHandler(req, rec); err != nil {
+		return err
+	}
+	if rec.Code != http.StatusOK {
+		var resp errorResp
+		if err := json.NewDecoder(rec.Body).Decode(&resp); err != nil {
+			return err
+		}
+		return &resp
+	}
+	c.cookies = rec.Result().Cookies()
+	return nil
+}
+
+func (c *testClient) Status() (Status, error) {
+	req := httptest.NewRequest(http.MethodGet, "/api/v0/status", nil)
+	c.setCookie(req)
+	rec := httptest.NewRecorder()
+	if err := testHandler(req, rec); err != nil {
+		return Status{}, err
+	}
+	if rec.Code != http.StatusOK {
+		var resp errorResp
+		if err := json.NewDecoder(rec.Body).Decode(&resp); err != nil {
+			return Status{}, err
+		}
+		return Status{}, &resp
+	}
+	var resp Status
+	if err := json.NewDecoder(rec.Body).Decode(&resp); err != nil {
+		return Status{}, err
+	}
+	return resp, nil
+}
--- api/users_test.go
+++ api/users_test.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"bytes"
 	"context"
 	"database/sql"
 	"encoding/json"
@@ -14,11 +13,29 @@ import (
 func TestUserLoginScenario(t *testing.T) {
 	testSetup(t)
 	defer testTeardown(t)
-	testRegisterUser(t, "test", "qwerty123")
+	client := newTestClient()
+	if _, err := client.Register("test", "qwerty123"); err != nil {
+		t.Fatal("Error:", err)
+	}
 	testSyncManagers(t)
 	testSocketObserveUser(t, "test")
 	testSocketObserveUserRoles(t, "test")
-	testLoginUser(t, "test", "qwerty123")
+	if _, err := client.Login("test", "qwerty123"); err != nil {
+		t.Fatal("Error:", err)
+	}
+	if status, err := client.Status(); err != nil {
+		t.Fatal("Error:", err)
+	} else {
+		if status.User == nil {
+			t.Fatal("Status should have user")
+		}
+		if status.Session == nil {
+			t.Fatal("Status should have session")
+		}
+	}
+	if err := client.Logout(); err != nil {
+		t.Fatal("Error:", err)
+	}
 }
 
 func testSyncManagers(tb testing.TB) {
@@ -42,58 +59,6 @@ func testSyncManagers(tb testing.TB) {
 	); err != nil {
 		tb.Fatal("Error:", err)
 	}
-}
-
-func testRegisterUser(tb testing.TB, login, password string) User {
-	data, err := json.Marshal(map[string]string{
-		"login":       login,
-		"password":    password,
-		"email":       "test@example.com",
-		"first_name":  "First",
-		"last_name":   "Last",
-		"middle_name": "Middle",
-	})
-	if err != nil {
-		tb.Fatal("Error:", err)
-	}
-	req := httptest.NewRequest(
-		http.MethodPost, "/api/v0/register", bytes.NewReader(data),
-	)
-	req.Header.Add("Content-Type", "application/json")
-	rec := httptest.NewRecorder()
-	if err := testHandler(req, rec); err != nil {
-		tb.Fatal("Error:", err)
-	}
-	expectStatus(tb, http.StatusCreated, rec.Code)
-	var resp User
-	if err := json.NewDecoder(rec.Body).Decode(&resp); err != nil {
-		tb.Fatal("Error:", err)
-	}
-	return resp
-}
-
-func testLoginUser(tb testing.TB, login, password string) Session {
-	data, err := json.Marshal(map[string]string{
-		"login":    login,
-		"password": password,
-	})
-	if err != nil {
-		tb.Fatal("Error:", err)
-	}
-	req := httptest.NewRequest(
-		http.MethodPost, "/api/v0/login", bytes.NewReader(data),
-	)
-	req.Header.Add("Content-Type", "application/json")
-	rec := httptest.NewRecorder()
-	if err := testHandler(req, rec); err != nil {
-		tb.Fatal("Error:", err)
-	}
-	expectStatus(tb, http.StatusCreated, rec.Code)
-	var resp Session
-	if err := json.NewDecoder(rec.Body).Decode(&resp); err != nil {
-		tb.Fatal("Error:", err)
-	}
-	return resp
 }
 
 func testSocketObserveUser(tb testing.TB, login string) User {

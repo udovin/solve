d65fdf83df7e6bbd49e43a866365982df9e646f1
Ivan Udovin <wilcot@ya.ru>
2022-01-05 14:47:01 +0300

    Add tests

--- config/config_test.go
+++ config/config_test.go
@@ -2,9 +2,11 @@ package config
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/labstack/gommon/log"
@@ -49,9 +51,59 @@ func TestLoadFromFile(t *testing.T) {
 	if err != nil {
 		t.Fatal("Error: ", err)
 	}
-	if string(configData) != string(expectedConfigData) {
-		t.Fatal("Config was corrupted")
+	testExpect(t, string(configData), string(expectedConfigData))
+}
+
+const templateConfig = `
+{
+	"server": {
+		"host": {{ "localhost" | json }},
+		"port": {{ 4242 | json }}
+	},
+	"db": {
+		"driver": "sqlite",
+		"options": {
+			"path": {{ file "SECRET_FILE" | json }}
+		}
 	}
+}
+`
+
+func TestLoadFromTemplateFile(t *testing.T) {
+	secretFile, err := ioutil.TempFile(os.TempDir(), "solve-test-secret-")
+	if err != nil {
+		t.Error("Error: ", err)
+	}
+	if _, err := secretFile.Write([]byte("secret")); err != nil {
+		t.Fatal("Error: ", err)
+	}
+	_ = secretFile.Close()
+	defer func() {
+		_ = os.Remove(secretFile.Name())
+	}()
+	file, err := ioutil.TempFile(os.TempDir(), "solve-test-")
+	if err != nil {
+		t.Error("Error: ", err)
+	}
+	if _, err = file.Write([]byte(strings.ReplaceAll(
+		templateConfig, "SECRET_FILE", secretFile.Name(),
+	))); err != nil {
+		t.Fatal("Error: ", err)
+	}
+	_ = file.Close()
+	defer func() {
+		_ = os.Remove(file.Name())
+	}()
+	cfg, err := LoadFromFile(file.Name())
+	if err != nil {
+		t.Fatal("Error: ", err)
+	}
+	if opts, ok := cfg.DB.Options.(SQLiteOptions); !ok {
+		t.Fatalf("Invalid options type: %T", cfg.DB.Options)
+	} else {
+		testExpect(t, opts.Path, "secret")
+	}
+	testExpect(t, cfg.LogLevel, log.INFO)
 }
 
 func TestLoadFromInvalidFile(t *testing.T) {
@@ -110,8 +162,14 @@ func TestLoadFromInvalidTemplateFile2(t *testing.T) {
 
 func TestServerAddress(t *testing.T) {
 	s := Server{Host: "localhost", Port: 8080}
-	addr := "localhost:8080"
-	if v := s.Address(); v != addr {
-		t.Fatalf("Expected %q, got %q", addr, v)
+	testExpect(t, s.Address(), "localhost:8080")
+}
+
+func testExpect[T comparable](tb testing.TB, output, answer T) {
+	if output != answer {
+		tb.Fatalf(
+			"Expected %q, got %q",
+			fmt.Sprint(answer), fmt.Sprint(output),
+		)
 	}
 }

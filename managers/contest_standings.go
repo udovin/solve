a5f22e50d1f65e72c5997e041e9c8630d16e2d98
Ivan Udovin <wilcot@ya.ru>
2023-01-23 02:30:43 +0300

    Add code for contest standings

--- managers/contest_standings.go
+++ managers/contest_standings.go
@@ -1,8 +1,9 @@
 package managers
 
 import (
+	"context"
 	"database/sql"
-	"fmt"
+	"sort"
 
 	"github.com/udovin/solve/core"
 	"github.com/udovin/solve/models"
@@ -11,6 +12,7 @@ import (
 type ContestStandingsManager struct {
 	contestParticipants *models.ContestParticipantStore
 	contestSolutions    *models.ContestSolutionStore
+	contestProblems     *models.ContestProblemStore
 	solutions           *models.SolutionStore
 }
 
@@ -18,43 +20,167 @@ func NewContestStandingsManager(core *core.Core) *ContestStandingsManager {
 	return &ContestStandingsManager{
 		contestParticipants: core.ContestParticipants,
 		contestSolutions:    core.ContestSolutions,
+		contestProblems:     core.ContestProblems,
 		solutions:           core.Solutions,
 	}
 }
 
-func (m *ContestStandingsManager) BuildStandings(ctx *ContestContext) (*ContestStandings, error) {
-	effectiveParticipant := ctx.GetEffectiveParticipant()
-	contestSolutions, err := m.contestSolutions.FindByContest(ctx.Contest.ID)
+func (m *ContestStandingsManager) BuildStandings(ctx context.Context, contest models.Contest) (*ContestStandings, error) {
+	contestConfig, err := contest.GetConfig()
 	if err != nil {
 		return nil, err
 	}
-	participants, err := m.contestParticipants.FindByContest(ctx.Contest.ID)
+	participants, err := m.contestParticipants.FindByContest(contest.ID)
 	if err != nil {
 		return nil, err
 	}
-	participantPos := map[int64]int{}
-	for i, participant := range participants {
-		participantPos[participant.ID] = i
+	contestProblems, err := m.contestProblems.FindByContest(contest.ID)
+	if err != nil {
+		return nil, err
 	}
-	for _, contestSolution := range contestSolutions {
-		pos, ok := participantPos[contestSolution.ParticipantID]
+	sortFunc(contestProblems, func(lhs, rhs models.ContestProblem) bool {
+		return lhs.Code < rhs.Code
+	})
+	var columns []ContestStandingsColumn
+	columnByProblem := map[int64]int{}
+	for i, problem := range contestProblems {
+		columns = append(columns, ContestStandingsColumn{
+			Problem: problem,
+		})
+		columnByProblem[problem.ID] = i
+	}
+	contestSolutions, err := m.contestSolutions.FindByContest(contest.ID)
+	if err != nil {
+		return nil, err
+	}
+	solutionsByParticipant := map[int64][]models.ContestSolution{}
+	for _, solution := range contestSolutions {
+		solutionsByParticipant[solution.ParticipantID] = append(
+			solutionsByParticipant[solution.ParticipantID], solution,
+		)
+	}
+	standings := ContestStandings{}
+	for _, participant := range participants {
+		beginTime := int64(contestConfig.BeginTime)
+		if participant.Kind == models.RegularParticipant {
+			var participantConfig models.RegularParticipantConfig
+			if err := participant.ScanConfig(&participantConfig); err != nil {
+				continue
+			}
+			if participantConfig.BeginTime != 0 {
+				beginTime = int64(participantConfig.BeginTime)
+			}
+		}
+		participantSolutions, ok := solutionsByParticipant[participant.ID]
 		if !ok {
 			continue
 		}
-		participant := participants[pos]
-		solution, err := m.solutions.Get(contestSolution.SolutionID)
-		if err != nil {
-			if err == sql.ErrNoRows {
+		sortFunc(participantSolutions, func(lhs, rhs models.ContestSolution) bool {
+			return lhs.ID < rhs.ID
+		})
+		solutionsByColumn := map[int][]models.Solution{}
+		for _, participantSolution := range participantSolutions {
+			solution, err := m.solutions.Get(participantSolution.SolutionID)
+			if err != nil {
+				if err == sql.ErrNoRows {
+					continue
+				}
+				return nil, err
+			}
+			column, ok := columnByProblem[participantSolution.ProblemID]
+			if !ok {
 				continue
 			}
-			return nil, fmt.Errorf("cannot get solution: %w", err)
+			solutionsByColumn[column] = append(solutionsByColumn[column], solution)
 		}
-		_ = participant
-		_ = solution
+		row := ContestStandingsRow{
+			Participant: participant,
+		}
+		for i := range columns {
+			solutions, ok := solutionsByColumn[i]
+			if !ok {
+				continue
+			}
+			cell := ContestStandingsCell{
+				Column: i,
+			}
+			for _, solution := range solutions {
+				report, err := solution.GetReport()
+				if err != nil {
+					continue
+				}
+				if report == nil {
+					cell.Attempt++
+					cell.Verdict = 0
+					break
+				}
+				if report.Verdict == models.CompilationError {
+					continue
+				}
+				cell.Attempt++
+				cell.Verdict = report.Verdict
+				if report.Verdict == models.Accepted {
+					cell.Time = solution.CreateTime - beginTime
+					if cell.Time < 0 {
+						cell.Time = 0
+					}
+					break
+				}
+			}
+			row.Cells = append(row.Cells, cell)
+		}
+		for _, cell := range row.Cells {
+			if cell.Verdict == models.Accepted {
+				row.Score++
+				row.Penalty = int64(cell.Attempt-1)*20 + cell.Time/60
+			}
+		}
+		standings.Rows = append(standings.Rows, row)
 	}
-	_ = effectiveParticipant
-	return nil, fmt.Errorf("not implemented")
+	return &standings, nil
+}
+
+type ContestStandingsColumn struct {
+	Problem models.ContestProblem
+}
+
+type ContestStandingsCell struct {
+	Column  int
+	Verdict models.Verdict
+	Attempt int
+	Time    int64
+}
+
+type ContestStandingsRow struct {
+	Participant models.ContestParticipant
+	Cells       []ContestStandingsCell
+	Score       int
+	Penalty     int64
 }
 
 type ContestStandings struct {
+	Columns []ContestStandingsColumn
+	Rows    []ContestStandingsRow
+}
+
+func sortFunc[T any](a []T, less func(T, T) bool) {
+	impl := sortFuncImpl[T]{data: a, less: less}
+	sort.Sort(&impl)
+}
+
+type sortFuncImpl[T any] struct {
+	data []T
+	less func(T, T) bool
+}
+
+func (s *sortFuncImpl[T]) Len() int {
+	return len(s.data)
+}
+
+func (s *sortFuncImpl[T]) Swap(i, j int) {
+	s.data[i], s.data[j] = s.data[j], s.data[i]
+}
+
+func (s *sortFuncImpl[T]) Less(i, j int) bool {
+	return s.less(s.data[i], s.data[j])
 }

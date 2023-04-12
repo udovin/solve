fedc2102ed65bc5e1bdf204bc17854585ec4e07b
Ivan Udovin <wilcot@ya.ru>
2023-04-12 23:14:25 +0300

    Add index

---
+++ models/index.go
@@ -0,0 +1,134 @@
+package models
+
+import (
+	"math"
+
+	"github.com/udovin/algo/btree"
+	"github.com/udovin/solve/db"
+)
+
+type btreeIndexKey[T any] struct {
+	Key T
+	ID  int64
+}
+
+type btreeIndex[K any, T any, TPtr db.ObjectPtr[T]] struct {
+	less func(K, K) bool
+	key  func(T) (K, bool)
+	m    btree.Map[btreeIndexKey[K], struct{}]
+}
+
+func newBTreeIndex[K comparable, T any, TPtr ObjectPtr[T]](
+	key func(T) (K, bool), less func(K, K) bool,
+) *btreeIndex[K, T, TPtr] {
+	return &btreeIndex[K, T, TPtr]{key: key, less: less}
+}
+
+func (i *btreeIndex[K, T, TPtr]) mapLess(lhs, rhs btreeIndexKey[K]) bool {
+	if i.less(lhs.Key, rhs.Key) {
+		return true
+	}
+	if i.less(rhs.Key, lhs.Key) {
+		return false
+	}
+	return lhs.ID < rhs.ID
+}
+
+func (i *btreeIndex[K, T, TPtr]) Reset() {
+	i.m = btree.NewMap[btreeIndexKey[K], struct{}](i.mapLess)
+}
+
+func (i *btreeIndex[K, T, TPtr]) Register(object T) {
+	key, ok := i.key(object)
+	if !ok {
+		return
+	}
+	id := TPtr(&object).ObjectID()
+	i.m.Set(btreeIndexKey[K]{Key: key, ID: id}, struct{}{})
+}
+
+func (i *btreeIndex[K, T, TPtr]) Deregister(object T) {
+	key, ok := i.key(object)
+	if !ok {
+		return
+	}
+	id := TPtr(&object).ObjectID()
+	i.m.Delete(btreeIndexKey[K]{Key: key, ID: id})
+}
+
+func (i *btreeIndex[K, T, TPtr]) Find(key K) *btreeIndexIter[K] {
+	return &btreeIndexIter[K]{
+		iter: i.m.Iter(),
+		key:  key,
+		less: i.less,
+	}
+}
+
+func (i *btreeIndex[K, T, TPtr]) ReverseFind(key K) *btreeIndexReverseIter[K] {
+	return &btreeIndexReverseIter[K]{
+		iter: i.m.Iter(),
+		key:  key,
+		less: i.less,
+	}
+}
+
+type indexIter interface {
+	Next() bool
+	ID() int64
+}
+
+type btreeIndexIter[K any] struct {
+	iter   btree.MapIter[btreeIndexKey[K], struct{}]
+	key    K
+	less   func(K, K) bool
+	seeked bool
+}
+
+func (i *btreeIndexIter[K]) Next() bool {
+	if !i.seeked {
+		i.seeked = true
+		if !i.iter.Seek(btreeIndexKey[K]{Key: i.key}) {
+			return false
+		}
+	} else {
+		if !i.iter.Next() {
+			return false
+		}
+	}
+	return !i.less(i.key, i.iter.Key().Key)
+}
+
+func (r *btreeIndexIter[K]) ID() int64 {
+	return r.iter.Key().ID
+}
+
+type btreeIndexReverseIter[K any] struct {
+	iter   btree.MapIter[btreeIndexKey[K], struct{}]
+	key    K
+	less   func(K, K) bool
+	seeked bool
+}
+
+func (i *btreeIndexReverseIter[K]) Next() bool {
+	if !i.seeked {
+		i.seeked = true
+		if !i.iter.Seek(btreeIndexKey[K]{Key: i.key, ID: math.MaxInt64}) {
+			if !i.iter.Last() {
+				return false
+			}
+		} else {
+			if !i.iter.Prev() {
+				return false
+			}
+		}
+	} else {
+		if !i.iter.Prev() {
+			return false
+		}
+	}
+	return !i.less(i.key, i.iter.Key().Key)
+}
+
+func (r *btreeIndexReverseIter[K]) ID() int64 {
+	return r.iter.Key().ID
+}
